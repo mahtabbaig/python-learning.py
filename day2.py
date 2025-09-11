7a98f0a517cd8111ceef8d76b4f0d7b0a3c818cf
mahtabbaig190@gmail.com <mahtabbaig190@gmail.com>
2025-09-11 13:21:13 +0530

    added new

--- day2.py
+++ day2.py
@@ -5,3 +5,16 @@ age = 19
 print("my name is:",name)
 print("my age is :",age)
 
+# Data types
+
+name = "mahtab"
+age  = 19
+percent = 99.98
+male =True
+prize =None
+
+print(type(name))
+print(type(age))
+print(type(percent))
+print(type(male))
+print(type(prize))

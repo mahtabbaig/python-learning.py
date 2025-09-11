81c1fe3dcbf672ffc52f622ab242896b36574fdf
mahtabbaig190@gmail.com <mahtabbaig190@gmail.com>
2025-09-11 14:19:52 +0530

    rename changes

--- day2.py
+++ day2.py
@@ -3,6 +3,8 @@
 name = "mahtab"
 
 age = 19
+print("my name is:",name)
+print("my age is:",age)
 
 # Data types
 
@@ -11,6 +13,12 @@ age  = 19
 percent = 99.98
 male =True
 prize =None
+
+print(type(name))
+print(type(age))
+print(type(percent))
+print(type(male))
+print(type(prize))
 
 # operators
 

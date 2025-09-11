5caa0f7140b94288ad356df19566126dc4723625
mahtabbaig190@gmail.com <mahtabbaig190@gmail.com>
2025-09-11 14:10:33 +0530

    new lines

--- day2.py
+++ day2.py
@@ -1,9 +1,8 @@
 #variable
 
 name = "mahtab"
+
 age = 19
-print("my name is:",name)
-print("my age is :",age)
 
 # Data types
 
@@ -13,8 +12,26 @@ percent = 99.98
 male =True
 prize =None
 
-print(type(name))
-print(type(age))
-print(type(percent))
-print(type(male))
-print(type(prize))
+# operators
+
+## Arithemetic
+a=2
+b=5
+diff=a-b
+print(diff)
+
+## relational
+a=2
+b=5
+print(a==b)
+print(a!=b)
+
+## Assignment
+a=5
+a**=10
+print(a)
+
+## logical
+a=5
+b=5
+print(not(a==b))

717c524ec71a030f5ed31b7e0433a37e319f7217
mahtabbaig190@gmail.com <mahtabbaig190@gmail.com>
2025-09-17 21:01:10 +0530

    new lesson added

--- day5.py
+++ day5.py
@@ -86,4 +86,59 @@ print(student)
 
 # Sets :
 
+collection={"mahtab",1,2}
+print(type(collection))
+print(collection)
+print(len(collection))
+
+##empty set:
+collection=set()
+print(collection)
+print(type(collection))
+
+## methods of set:
+### 1.add:
+
+collection={"mahtab",1,2}
+collection.add(4)
+print(collection)
+
+### 2. remove:
+
+collection={"mahtab",1,2}
+collection.remove("mahtab")
+print(collection)
+
+### 3. clear:
+
+collection={"mahtab",1,2}
+collection.clear()
+print(collection)
+
+### 4. pop:
+
+collection={"mahtab",1,2}
+collection.pop()
+print(collection)
+print(collection.pop())
+
+### 5.union:
+
+collection1={"mahtab",1,2}
+collection2={2,3,1}
+print(collection1.union(collection2))
+
+### 6.intersection:
+
+
+collection1={"mahtab",1,2}
+collection2={1,1,2}
+print(collection1.intersection(collection2))
+
+
+
+
+
+
+
 

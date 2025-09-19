4675df872c4889bf4e8cfdd26ec0aae24e64958b
mahtabbaig190@gmail.com <mahtabbaig190@gmail.com>
2025-09-19 23:32:08 +0530

    range concept added

--- day6.py
+++ day6.py
@@ -17,3 +17,17 @@ num=[1,2,3,4,5]
 for value in num:
     print(value)
     
+# Range:
+for i in range(5):
+    print(i)    
+
+for i in range(1,5):
+    print(i)
+
+for i in range(1,10,2) :
+    print(i)   
+
+# pass statement:
+for i in range(5):
+    pass
+print("later")    

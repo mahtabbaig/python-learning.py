2d32e310e28d0831eee229bccd9929199699a1cb
mahtabbaig190@gmail.com <mahtabbaig190@gmail.com>
2025-09-25 22:47:49 +0530

    function concept added

--- day7.py
+++ day7.py
@@ -4,9 +4,43 @@ def average_(a,b,c):  #to find average of 3 num:
      sum=a+b+c
      average=sum/3
      print(average)
+     
 
 average_(30,23,4) 
-average_(1,2,3)    
+average_(1,2,3)   
+
+
+def multiple(a):
+     sum=a**2
+     print(sum)
+     return(sum)
+
+multiple(5)
+
+
+i=6
+if(i>=6):
+     print(multiple(6))
+else:
+     print("not valid") 
+
+multiple(12)         
+
+average_(3,4,5)
+
+## default fun:
+
+def div(a=1,b=3):
+     print(a/b)
+     return(a/b)
+div()
+
+def sub(a,b=3):
+     print(a-b)
+     return(a-b)
+sub(2)
+     
+
 
 
 

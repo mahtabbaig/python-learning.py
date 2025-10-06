1b28e049ee8f79fa51094c67ad17ef0d75911adb
mahtabbaig190@gmail.com <mahtabbaig190@gmail.com>
2025-10-07 00:17:32 +0530

    file reading

--- day8.py
+++ day8.py
@@ -1,4 +1,18 @@
 #Files I/O in python:
+1.
+f=open("day1.py","r")
 
-import os
-print(os.getcwd())
+data=f.read()
+print(data)
+
+f.close()
+
+2.
+f=open("day1.py","r")
+
+data=f.read()
+print(data)
+line1=f.readline()
+line2=f.readline()
+print(line1,line2)
+f.close()

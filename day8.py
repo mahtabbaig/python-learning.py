e158f4d4f8b14240ccb0e25464c825149bffd4dd
mahtabbaig190@gmail.com <mahtabbaig190@gmail.com>
2025-10-07 22:38:06 +0530

    modes and delete file

--- day8.py
+++ day8.py
@@ -1,4 +1,6 @@
 #Files I/O in python:
+
+##file reading
 1.
 f=open("day1.py","r")
 
@@ -16,3 +18,34 @@ line1=f.readline()
 line2=f.readline()
 print(line1,line2)
 f.close()
+
+## file writing
+with open("day1.py","w")as f:
+    data=f.write("mahtab")
+    
+
+
+
+## file append
+with open("day1.py","a")as f:
+    data=f.write("azeez")
+    
+## file r+
+with open("day1.py","r+")as f:
+    data=f.write("brother")
+    
+## file w+
+with open("day1.py","w+")as f:
+
+    data=f.read()
+    print(data)
+
+#deleting file
+
+import os
+os.remove("day9.py")
+
+    
+
+ 
+
